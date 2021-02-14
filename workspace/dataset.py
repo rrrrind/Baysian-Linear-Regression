72997506407a02df491983a86754e8b11d65d9ee
rrrrind <masaya.mori0905@gmail.com>
2021-02-14 15:43:53 +0900

    Add function of x_range

--- workspace/dataset.py
+++ workspace/dataset.py
@@ -2,14 +2,26 @@ import numpy as np
 
 class ObservationData(object):
     def __init__(self, mu, std, sampling):
-        np.random.seed(3)
-        self.mu = mu
-        self.std = std
-        self.sampling = sampling
+        self.x = None
+        self.y = None
+        self.generate(mu, std, sampling)
     
-    def generate(self):
-        x_scalar = np.random.normal(self.mu, self.std, self.sampling)
-        # 正規分布
-        y = ((np.exp((-(x_scalar - self.mu)**2)/(2*(self.std**2))) / (np.sqrt(2*np.pi)*self.std))
-          + 0.05*np.random.randn(len(x_scalar)))
-        return x_scalar, y
+    def generate(self, mu, std, sampling):
+        # 正規分布に従いサンプリング
+        self.x = np.random.normal(mu, std, sampling)
+        # 残差
+        residual = 0.05 * np.random.randn(len(self.x))
+        # 正規分布 + 残差
+        self.y = ((np.exp((-(self.x - mu)**2)/(2*(std**2))) / (np.sqrt(2*np.pi)*std)) + residual)
+        
+    @property
+    def x_org(self):
+        return self.x
+    
+    @property
+    def y_org(self):
+        return self.y
+    
+    @property
+    def x_range(self):
+        return np.arange(np.min(self.x)-0.5, np.max(self.x)+0.5, 0.02)
