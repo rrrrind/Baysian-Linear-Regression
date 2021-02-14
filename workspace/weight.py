086d526ec74b09c3e18b417c6ed80ef63e01e721
rrrrind <masaya.mori0905@gmail.com>
2021-02-14 15:44:35 +0900

    Mod values name

--- workspace/weight.py
+++ workspace/weight.py
@@ -1,7 +1,10 @@
 import numpy as np
+import preprocessing as pp
 
-class WeightDistribution(object):
+class MultivariateNormalDistribution(object):
     def __init__(self, term_num):
+        self.term_num = term_num
+        
         self.prior_mean = np.zeros(term_num)
         self.prior_cov = np.ones([term_num,term_num])
 
@@ -13,14 +16,16 @@ class WeightDistribution(object):
     def prior_cov(self):
         return self.prior_cov
 
-    def _calc_posterior_cov(self, x_vec, get_y_sigma()):
-        return (np.dot(x_vec, x_vec.T) / get_y_sigma()) + (1/self.prior_cov)
+    def _calc_posterior_cov(self, x_org_scalar, initial_sigma):
+        x_vec = pp.trans_scalar_to_vec(x_org_scalar, term_num=self.term_num)
+        return (np.dot(x_vec, x_vec.T) / initial_sigma) + (1/self.prior_cov)
 
-    def _calc_posterior_mean(self, posterior_cov, x_vec, y, get_y_sigma()):
-        return posterior_cov * (((y@x_vec.T)/get_y_sigma()) + (1/self.prior_cov)@self.prior_mean)
+    def _calc_posterior_mean(self, posterior_cov, x_org_scalar, y_org, initial_sigma):
+        x_vec = pp.trans_scalar_to_vec(x_org_scalar, term_num=self.term_num)
+        return posterior_cov * (((y@x_vec.T)/initial_sigma) + (1/self.prior_cov)@self.prior_mean)
 
-    def update_params(self, x_vec, y, get_y_sigma()):
-        posterior_cov = self._calc_weight_cov(x_vec, get_y_sigma())
-        posterior_mean = self._calc_weight_mean(posterior_cov, x_vec, y, get_y_sigma())
-        self.prior_cov = new_weight_cov
-        self.prior_mean = new_weight_mean
+    def update_params(self, x_org_scalar, y_org, initial_sigma):
+        posterior_cov = self._calc_posterior_cov(x_org_scalar, initial_sigma)
+        posterior_mean = self._calc_posterior_mean(posterior_cov, x_org_scalar, y_org, initial_sigma)
+        self.prior_cov = posterior_cov
+        self.prior_mean = posterior_mean
