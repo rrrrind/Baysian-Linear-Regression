cec8afe6c23b8652489595d5818419151d12aaa0
rrrrind <masaya.mori0905@gmail.com>
2021-02-14 18:18:22 +0900

    Mod update_params

--- workspace/weight.py
+++ workspace/weight.py
@@ -5,27 +5,30 @@ class MultivariateNormalDistribution(object):
     def __init__(self, term_num):
         self.term_num = term_num
         
-        self.prior_mean = np.zeros(term_num)
+        self.prior_mean = np.ones([term_num, 1])
         self.prior_cov = np.ones([term_num,term_num])
+        self.prior_acc = np.linalg.pinv(self.prior_cov)
 
     @property
-    def prior_mean(self):
+    def mean(self):
         return self.prior_mean
 
     @property
-    def prior_cov(self):
+    def cov(self):
         return self.prior_cov
 
-    def _calc_posterior_cov(self, x_org_scalar, initial_sigma):
+    def _calc_posterior_acc(self, x_org_scalar, initial_sigma):
         x_vec = pp.trans_scalar_to_vec(x_org_scalar, term_num=self.term_num)
-        return (np.dot(x_vec, x_vec.T) / initial_sigma) + (1/self.prior_cov)
+        return (initial_sigma * np.dot(x_vec, x_vec.T)) + self.prior_acc
 
-    def _calc_posterior_mean(self, posterior_cov, x_org_scalar, y_org, initial_sigma):
+    def _calc_posterior_mean(self, posterior_acc, x_org_scalar, y_org, initial_sigma):
         x_vec = pp.trans_scalar_to_vec(x_org_scalar, term_num=self.term_num)
-        return posterior_cov * (((y@x_vec.T)/initial_sigma) + (1/self.prior_cov)@self.prior_mean)
+        return np.dot(np.linalg.pinv(posterior_acc), 
+                     ((initial_sigma*y_org*x_vec)+np.dot(self.prior_acc, self.prior_mean)))
 
     def update_params(self, x_org_scalar, y_org, initial_sigma):
-        posterior_cov = self._calc_posterior_cov(x_org_scalar, initial_sigma)
-        posterior_mean = self._calc_posterior_mean(posterior_cov, x_org_scalar, y_org, initial_sigma)
-        self.prior_cov = posterior_cov
+        posterior_acc = self._calc_posterior_acc(x_org_scalar, initial_sigma)
+        posterior_mean = self._calc_posterior_mean(posterior_acc, x_org_scalar, y_org, initial_sigma)
+        self.prior_acc = posterior_acc
+        self.prior_cov = np.linalg.inv(posterior_acc)
         self.prior_mean = posterior_mean
