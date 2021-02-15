0674ccced15efed299b7749653c3c05be395c549
rrrrind <masaya.mori0905@gmail.com>
2021-02-15 18:43:06 +0900

    Fix initial_sigma

--- workspace/weight.py
+++ workspace/weight.py
@@ -19,16 +19,16 @@ class MultivariateNormalDistribution(object):
 
     def _calc_posterior_acc(self, x_org_scalar, initial_sigma):
         x_vec = pp.trans_scalar_to_vec(x_org_scalar, term_num=self.term_num)
-        return (initial_sigma * np.dot(x_vec, x_vec.T)) + self.acc
+        return ((1/initial_sigma) * np.dot(x_vec, x_vec.T)) + self.acc
 
     def _calc_posterior_mean(self, posterior_acc, x_org_scalar, y_org, initial_sigma):
         x_vec = pp.trans_scalar_to_vec(x_org_scalar, term_num=self.term_num)
         return np.dot(np.linalg.pinv(posterior_acc), 
-                     ((initial_sigma*y_org*x_vec)+np.dot(self.acc, self.mean)))
+                     (((1/initial_sigma)*y_org*x_vec)+np.dot(self.acc, self.mean)))
 
     def update_params(self, x_org_scalar, y_org, initial_sigma):
         posterior_acc = self._calc_posterior_acc(x_org_scalar, initial_sigma)
         posterior_mean = self._calc_posterior_mean(posterior_acc, x_org_scalar, y_org, initial_sigma)
         self.acc = posterior_acc
-        self.cov = np.linalg.inv(posterior_acc)
+        self.cov = np.linalg.pinv(posterior_acc)
         self.mean = posterior_mean
